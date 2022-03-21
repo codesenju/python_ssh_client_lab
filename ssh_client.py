f38e4870d47417d6b8e180b381206c26a3e7c212
Lehlogonolo Masubelele <l.masubelele@gmail.com>
2022-03-21 13:53:07 +0200

    initial commit

---
+++ ssh_client.py
@@ -0,0 +1,31 @@
+import paramiko
+
+hostname = 'localhost'
+username = 'codesenju'
+password = 'passw0rd'
+PORT = '222'
+ssh = paramiko.SSHClient()
+
+
+def execRemoteCommand(cmd, client):
+    stdin, stdout, stderr = client.exec_command(cmd)
+    output = stderr.readlines()
+    for index, item in enumerate(output):
+        print(item)
+    output = stdout.readlines()
+    for index, item in enumerate(output):
+        print(item)
+
+try:
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=hostname, username=username, password=password,port=PORT)
+#    sftp_client = ssh_client.open_sftp()
+    cmd = 'ls -l'
+    print(cmd)
+    execRemoteCommand(cmd,ssh_client)
+except Exception as err:
+    print("SSH CLIENT ERROR: {}" .format(err))
+finally:
+#    sftp_client.close()
+    ssh_client.close()
